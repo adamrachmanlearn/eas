2c373a82af1eb403a329911f763ffea425fca787
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-22 14:44:20 +0700

    Add event listener

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for (let i = 256; i > 0; i--) {
     const newDiv = document.createElement("div");
     container.appendChild(newDiv);
 }
+
+const divs = document.querySelectorAll(".container div");
+divs.forEach((div) => {
+    div.addEventListener("mouseover", (elem) => {
+        elem.target.style.backgroundColor = "black";
+    })
+})
