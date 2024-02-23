88f53cd652bc8d3cfa95f0753675aefdf157326b
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-23 14:11:19 +0700

    Add event delegation

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const container = document.querySelector(".container");
 for (let i = 256; i > 0; i--) {
     const newDiv = document.createElement("div");
     container.appendChild(newDiv);
-}
+};
 
-const divs = document.querySelectorAll(".container div");
-divs.forEach((div) => {
-    div.addEventListener("mouseover", (elem) => {
-        elem.target.style.backgroundColor = "black";
-    })
-})
+container.addEventListener("mouseover", element => {
+    if (element.target.tagName === "DIV") {
+        element.target.style.backgroundColor = "black";
+    }
+});
