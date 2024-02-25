4416b81399d4f80c64b01019fa18bf17589197ae
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-25 09:36:00 +0700

    Clean script.js

--- script.js
+++ script.js
@@ -41,5 +41,5 @@ function randomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return "rgb(" + r +", " + g + ", " + b + ")"
+    return "rgb(" + r + ", " + g + ", " + b + ")"
 }
