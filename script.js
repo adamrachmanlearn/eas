9c1849ecc71f910ba3c165e6acf3d90cfe5a91c9
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-25 09:32:09 +0700

    Edit script.js to make trail random color

--- script.js
+++ script.js
@@ -33,6 +33,13 @@ function createDiv(num) {
 
 container.addEventListener("mouseover", element => {
     if (element.target.className !== "container") {
-        element.target.style.backgroundColor = "black";
+        element.target.style.backgroundColor = randomColor();
     }
 });
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return "rgb(" + r +", " + g + ", " + b + ")"
+}
